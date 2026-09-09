d4fbb1e33178317122de99f4a6a7f911860bd53a
xuongduc <phuongxuongduc29052005@gmail.com>
2026-09-09 11:23:14 +0700

    sua va them two sum 3

--- Complex bai tap.cpp
+++ Complex bai tap.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+
+
 int max(int n[], int size){
     int t = 0;
     for (int i = 0; i < size ; ++i){
@@ -27,7 +30,7 @@ int TwoSum2(int n[], int s, int size){
     sort(n, n + size);
     int ct1 = 0;
     int ct2 = size - 1;
-    while (ct1 != ct2){
+    while (ct1 != ct2){ // xem lai dieu kien nay
         int target = s - n[ct1];
         if (target == n[ct2]){
             cout << n[ct1] << " " << n[ct2] << '\n';
@@ -42,9 +45,56 @@ int TwoSum2(int n[], int s, int size){
     }
     return v;
 }
+
+struct pairr {
+    int index;
+    int val;
+};
+bool compareP(const pairr &a, const pairr &b){
+    return a.val < b.val;
+}
+
+int TwoSum3(int n[], int s, int size){
+    int v = 0;
+
+    pairr save[size];
+    for (int i = 0; i < size; i++){
+        save[i].index = i;
+        save[i].val = n[i]; 
+    }
+
+
+    sort(save, save+size, compareP);
+
+    int ct1 = 0;
+    int ct2 = size - 1;
+
+    while (ct1 < ct2){
+        int target = s - save[ct1].val;
+        if (target == save[ct2].val){
+            
+           cout << save[ct1].index << " " << save[ct2].index << '\n'; 
+            
+            ct1++;
+            ct2--;
+            v++;
+
+        } else if (save[ct2].val > target ){
+            ct2--;
+            
+        }
+        else ct1++;
+
+    }
+
+    return v;
+}
+
+
+
 int main() {
-    int size = 1000;
-    int s = 10; // Giá trị tổng cần tìm (có thể thay đổi tùy ý)
+    int size = 100000;
+    int s = 100000; // Giá trị tổng cần tìm (có thể thay đổi tùy ý)
 
     // Khởi tạo mảng động 1000 phần tử để tránh tràn bộ nhớ stack
     int* n = new int[size];
@@ -55,7 +105,7 @@ int main() {
     }
 
     // Gọi hàm TwoSum
-    int result = TwoSum2(n, s, size);
+    int result = TwoSum3(n, s, size);
 
     cout << "So cap thoa man: " << result << endl;
 

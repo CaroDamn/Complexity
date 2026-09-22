9060df4b2c43e2fd0dad0a6ce24605e4213093da
CaroDamn <copthoht@gmail.com>
2026-09-22 19:35:27 +0700

    22/9

--- Complex bai tap.cpp
+++ Complex bai tap.cpp
@@ -27,7 +27,7 @@ int TwoSum2(int n[], int s, int size){
     sort(n, n + size);
     int ct1 = 0;
     int ct2 = size - 1;
-    while (ct1 != ct2){
+    while (ct1 < ct2){
         int target = s - n[ct1];
         if (target == n[ct2]){
             cout << n[ct1] << " " << n[ct2] << '\n';
@@ -42,25 +42,97 @@ int TwoSum2(int n[], int s, int size){
     }
     return v;
 }
-int main() {
-    int size = 1000;
-    int s = 10; // Giá trị tổng cần tìm (có thể thay đổi tùy ý)
+struct paire {
+    int index;
+    int val;
+};
 
-    // Khởi tạo mảng động 1000 phần tử để tránh tràn bộ nhớ stack
-    int* n = new int[size];
+bool compareP(const paire &a, const paire &b){
+    return a.val < b.val;
+}
 
-    // Gán dữ liệu mẫu cho mảng (ví dụ: các số từ 0 đến 999)
-    for (int i = 0; i < size; ++i) {
-        n[i] = i;
+int TwoSum3(int n[], int s, int size){
+    int ct1 = 0;
+    int ct2 = size - 1;
+    int v = 0;
+    paire save[size];
+    for (int i = 0; i< size; ++i){
+        save[i].index = i;
+        save[i].val = n[i]; 
     }
+    sort(save, save + size, compareP);
+    while (ct1 < ct2){
+        int target = s - save[ct1].val;
+        if (target == save[ct2].val){
+            cout << save[ct1].index << " " << save[ct2].index << '\n';
+            ++ct1;
+            --ct2;
+            ++v;
+        }else if (target > save[ct2].val){
+            ++ct1;
+        }else{
+            --ct2;
+        }
+    }
+    return v;
+}
 
-    // Gọi hàm TwoSum
-    int result = TwoSum2(n, s, size);
-
-    cout << "So cap thoa man: " << result << endl;
-
-    // Giải phóng bộ nhớ
-    delete[] n;
-
-    return 0;
+int missNum(int a[], int n){
+    int t = ((n + 1)* (n))/2;
+    for (int i = 0; i < n; ++i){
+        t -= a[i];
+    }
+    return t;
+}
+struct luu{
+    int gt = INT_MIN;
+    int bd;
+    int kt;
+};
+luu bien;
+int sum1(int p[], int a, int b){
+    int t = 0;
+    for (int i = a; i <= b; ++i){
+        t += p[i];
+    }
+    return t;
+}
+int kadane(int a[], int n){
+    int t;
+    for (int i = 0; i < n; ++i){
+        for (int j = n -1; j > i; --j){
+            t = sum1(a, i, j);
+            if (bien.gt < t){
+                bien.bd = i;
+                bien.kt = j;
+                bien.gt = t;
+            }
+        }
+    }
+    cout << bien.bd << " " << bien.kt;
+    return bien.gt;
+}
+int kadane1(int a[], int n){
+    int curMax = a[0];
+    int gloMax = a[0];
+    int tempbd;
+    for (int i = 1; i < n; ++i){
+        if (a[i] > a[i] + curMax){
+            tempbd = i;
+            curMax = a[i];
+        }else{
+            curMax = a[i] + curMax;
+        }
+        if (gloMax < curMax){
+            bien.bd = tempbd;
+            bien.kt = i;
+            gloMax = curMax;
+        }
+    }
+    cout << bien.bd << " " << bien.kt << '\n';
+    return gloMax;
+}
+int main() {
+  int a[] = {-21, -5, -9, -10, 1, 5, 6, 7, -2, -3, -4};
+  cout<< kadane1(a, 11);
 }
